b9345df01580a1b07fa20572a7283f864d2f152e
Nelio Alves <acenelio@gmail.com>
2018-07-28 21:16:25 -0300

    Inline lambda expression

--- Course/Program.cs
+++ Course/Program.cs
@@ -12,9 +12,7 @@ namespace Course {
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-
-            list.Sort(comp);
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
             foreach (Product p in list) {
                 Console.WriteLine(p);
