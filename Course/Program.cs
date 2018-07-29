f054e6b7ade5c34cc9cc580cf72f14cc2ca1a84e
Nelio Alves <acenelio@gmail.com>
2018-07-28 21:15:59 -0300

    Lambda expression to delegate variable

--- Course/Program.cs
+++ Course/Program.cs
@@ -12,17 +12,13 @@ namespace Course {
             list.Add(new Product("Notebook", 1200.00));
             list.Add(new Product("Tablet", 450.00));
 
-            Comparison<Product> comp = CompareProducts;
+            Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
             list.Sort(comp);
 
             foreach (Product p in list) {
                 Console.WriteLine(p);
             }
-        }
-
-        static int CompareProducts(Product p1, Product p2) {
-            return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
     }
 }
