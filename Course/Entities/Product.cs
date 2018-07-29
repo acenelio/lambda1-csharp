c3b97ae14171d95c1c441490a361d9f3c89d61e9
Nelio Alves <acenelio@gmail.com>
2018-07-28 21:11:05 -0300

    IComparable implementation

---
+++ Course/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course.Entities {
+    class Product : IComparable<Product> {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString() {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(Product other) {
+            return Name.ToUpper().CompareTo(other.Name.ToUpper());
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Course.Entities;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            list.Sort();
+
+            foreach (Product p in list) {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
